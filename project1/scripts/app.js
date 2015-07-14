85ce098630d07c58ad9f3e249b013445c0dc4cce
Matt Herron <matthew.herron77@gmail.com>
2015-07-13 23:34:12 -0500

    Added project file templates and wireframe draft

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
   console.log("All resources are loaded");
-
+  $('#form_send').on('click', function() {
+    var formdata = app.createFormObject();
+    console.log('Clicked the submit button...');
+  });
 });
+
+var app = app || {};
+
+app.createFormObject = function() {
+  var retJson = new Object();
+
+  retJson.username = $('#user_name').val();
+  console.log(retJson.username);
+  retJson.email = $('#user_email').val();
+  retJson.request = $('#user_request').val();
+
+  console.log(retJson);
+
+  return retJson;
+}
